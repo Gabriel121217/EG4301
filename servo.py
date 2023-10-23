a0fc63fcf4540171e3aa1c678b3a5a3e1ad90089
Gabriel <gab@Gabriels-MacBook-Pro.local>
2023-10-23 14:57:15 +0800

    lock

--- servo.py
+++ servo.py
@@ -1,22 +1,26 @@
 from gpiozero import AngularServo
 from time import sleep
+import RPi.GPIO as GPIO
+import time
 
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+# switch
+GPIO.setmode(GPIO.BCM)                
+pushpin = 21                                                  
+GPIO.setup(pushpin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # using the internal Pull up resistor
+
+status = True
 
 while (True):
-    servo.angle = 90
-    sleep(1)
-    servo.angle = 0
-    sleep(1)
-    servo.angle = -90
-    sleep(1)
-
-
-def lock(login_status):
-    if login_status == True:
-        servo.angle = 90
-    else:
-        servo.angle = 0
-
-
-        
+    if GPIO.input(pushpin) == 0:
+        if status:
+            servo.angle = 90
+            time.sleep(1)
+            servo.angle = 0
+            time.sleep(1)
+            servo.angle = -90
+            time.sleep(1)
+            status = False
+        else:
+            print("locked")
+            status = True
