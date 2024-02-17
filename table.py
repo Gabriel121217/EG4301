94f40fb6a761c26c551f317eb7a1513e6c217ace
Gabriel121217 <gbrlwong@gmail.com>
2024-02-17 10:49:08 +0800

    “comment”

--- table.py
+++ table.py
@@ -1,21 +1,24 @@
+#Import all neccessary features to code.
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
-# Set up GPIO
+#If code is stopped while the solenoid is active it stays active
+#This may produce a warning if the code is restarted and it finds the GPIO Pin, which it defines as non-active in next line, is still active
+#from previous time the code was run. This line prevents that warning syntax popping up which if it did would stop the code running.
+GPIO.setwarnings(False)
+#This means we will refer to the GPIO pins
+#by the number directly after the word GPIO. A good Pin Out Resource can be found here https://pinout.xyz/
 GPIO.setmode(GPIO.BCM)
+#This sets up the GPIO 18 pin as an output pin
 GPIO.setup(14, GPIO.OUT)
 
-def set_gpio_high():
-    GPIO.output(14,1)
-
-def set_gpio_low():
-    GPIO.output(14,0)
-
-# Example usage
-for i in range(10):
-    set_gpio_high()  # Call this function to set GPIO pin 14 to high
-    time.sleep(2)    # Sleep for 2 seconds (for demonstration purposes)
-    set_gpio_low()   # Call this function to set GPIO pin 14 to low
-
-# Cleanup GPIO
-GPIO.cleanup()
+while (True):    
+    
+    #This Turns Relay Off. Brings Voltage to Max GPIO can output ~3.3V
+    GPIO.output(14, 1)
+    #Wait 1 Seconds
+    sleep(1)
+    #Turns Relay On. Brings Voltage to Min GPIO can output ~0V.
+    GPIO.output(14, 0)
+    #Wait 1 Seconds
+    sleep(1)
