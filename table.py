f22a53d7827a62058c8196b1dd64351fc9ed1328
Gabriel121217 <gbrlwong@gmail.com>
2024-02-17 10:42:30 +0800

    “comment”

--- table.py
+++ table.py
@@ -1,35 +1,20 @@
-#Import all neccessary features to code.
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-#If code is stopped while the solenoid is active it stays active
-#This may produce a warning if the code is restarted and it finds the GPIO Pin, which it defines as non-active in next line, is still active
-#from previous time the code was run. This line prevents that warning syntax popping up which if it did would stop the code running.
-GPIO.setwarnings(False)
-#This means we will refer to the GPIO pins
-#by the number directly after the word GPIO. A good Pin Out Resource can be found here https://pinout.xyz/
+# Set up GPIO
 GPIO.setmode(GPIO.BCM)
-#This sets up the GPIO 18 pin as an output pin
 GPIO.setup(23, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
 
+def set_gpio_high():
+    GPIO.output(23, GPIO.HIGH)
 
-def moveup():
-    GPIO.output(23, 1)
+def set_gpio_low():
+    GPIO.output(23, GPIO.LOW)
 
-def movedown():
-    GPIO.output(24, 1)
+# Example usage
+set_gpio_high()  # Call this function to set GPIO pin 14 to high
+time.sleep(2)    # Sleep for 2 seconds (for demonstration purposes)
+set_gpio_low()   # Call this function to set GPIO pin 14 to low
 
-def stop():
-    GPIO.output(23, 0)
-    GPIO.output(24, 0)
-
-while (True):    
-    # moveup()
-    # sleep(2)
-    # stop()
-    sleep(1)
-    movedown()
-    sleep(2)
-    stop()
-
+# Cleanup GPIO
+GPIO.cleanup()
