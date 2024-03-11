1801f0aadc97f811c542f72cb914e198b40eacd5
Gabriel121217 <gbrlwong@gmail.com>
2024-03-11 10:44:58 +0800

    “comment”

--- servo.py
+++ servo.py
@@ -2,11 +2,14 @@ from gpiozero import Servo
 from time import sleep
 
 servo = Servo(22)
-val = -1
 
-while True:
-    servo.value = val
-    sleep(0.1)
-    val = val + 0.1
-    if val > 1:
-        val = -1
+def lock():
+    servo.value = 0.88
+
+def unlock():
+    servo.value = 0
+
+for i in range(5):
+    lock()
+    sleep(2)
+    unlock ()
