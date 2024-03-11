63170b1bb618b81171e457f9df4d568057f0883e
Gabriel121217 <gbrlwong@gmail.com>
2024-03-11 10:47:27 +0800

    “comment”

--- servo.py
+++ servo.py
@@ -10,6 +10,9 @@ def unlock():
     servo.min()
 
 for i in range(5):
-    lock()
-    sleep(2)
-    unlock ()
+    servo.min()
+    sleep(0.5)
+    servo.mid()
+    sleep(0.5)
+    servo.max()
+    sleep(0.5)
